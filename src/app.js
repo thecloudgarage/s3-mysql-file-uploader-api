0dde788ac4be21cfd670b9aeae4ad637cab865ba
ahmedmeshref <a.meshref@alustudent.com>
2021-03-17 22:01:14 +0200

    fix server import app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const axios = require('axios');
 var bodyParser = require('body-parser');
-
 const routes = require('./routes');
 const db = require("./database/models/");
 
@@ -20,4 +19,4 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api/v1', routes);
 
-
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
-import app from './app';
-
+const app = require('./app');
 
 const port = process.env.PORT || 80;
-
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
